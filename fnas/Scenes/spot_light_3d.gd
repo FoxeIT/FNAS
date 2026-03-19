50fe13082ea516aad4ec5ecb39058d6b53b83e87
Mint <mici5dabestpl@gmail.com>
2026-03-19 17:59:43 +0100

    👍
    👍

---
+++ fnas/Scenes/spot_light_3d.gd
@@ -0,0 +1,11 @@
+extends SpotLight3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(get_parent().get_node("CharacterBody3D").position)
