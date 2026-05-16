f101f47f7e59ba9d83317f949cdb6590112db664
MintPixelsLol <mici5dabestpl@gmail.com>
2026-05-16 15:48:39 +0200

    added a scanline

--- fnas/testy/asciiintro.gd
+++ fnas/testy/asciiintro.gd
@@ -42,12 +42,14 @@ extends Node3D
 ########################################"""
 @onready var animPlayer = $AnimationPlayer
 @onready var animPlayer2 = $AnimationPlayer2
+@onready var animPlayer3 = $AnimationPlayer3
 @onready var label = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animPlayer.play("RESET") # Replace with function body.
 	animPlayer.play("intro")
 	animPlayer2.play("smoke anim")
+	#animPlayer3.play("scanline")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
